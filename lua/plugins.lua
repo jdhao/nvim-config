ba490516e79dcf6e2ed77b58fddd2fb81dcb370c
jdhao <jdhao@hotmail.com>
2021-07-18 18:00:40 +0800

    Fix fugitive git index related error for packer.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -291,6 +291,6 @@ require('packer').startup(
 vim.api.nvim_exec([[
   augroup packer_auto_compile
     autocmd!
-    autocmd BufWritePost */lua/plugins.lua source <afile> | PackerCompile
+    autocmd BufWritePost */nvim/lua/plugins.lua source <afile> | PackerCompile
   augroup END
 ]], false)
