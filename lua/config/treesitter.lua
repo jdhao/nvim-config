e4cfefe76b61457142acbc1dac9dbf1cb98b5bd4
jdhao <jdhao@hotmail.com>
2021-07-30 01:50:20 +0800

    chore: remove trailing sapces

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,8 +1,8 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {'python', 'cpp', 'lua', 'bash'},
   ignore_install = {},  -- List of parsers to ignore installing
-  highlight = {            
+  highlight = {
     enable = true,  -- false will disable the whole extension
     disable = {},  -- list of language that will be disabled
-  },                                                   
-}   
+  },
+}
