36fa9f91e18de484fe71d819e6a1456bfff6162a
jdhao <jdhao@hotmail.com>
2021-08-02 00:25:25 +0800

    Add update script for macOS

---
+++ docs/nvim_install_mac.sh
@@ -0,0 +1,17 @@
+set -eux
+
+wget https://hub.fastgit.org/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+
+if [[ ! -d "$HOME/tools/"  ]]; then
+    mkdir -p "$HOME/tools"
+fi
+
+# Delete existing nvim installation.
+if [[ -d $HOME/tools/nvim-osx64/ ]]; then
+    rm -rf $HOME/tools/nvim-osx64
+fi
+
+# Extract the tar ball
+tar zxvf nvim-macos.tar.gz -C $HOME/tools
+
+rm nvim-macos.tar.gz
