9f2b8460118357167430d547f3445675ebb6757c
jdhao <jdhao@hotmail.com>
2021-08-23 22:50:30 +0800

    Add treesitter parser for vim script again

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = {"python", "cpp", "lua"},
+  ensure_installed = {"python", "cpp", "lua", "vim"},
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
