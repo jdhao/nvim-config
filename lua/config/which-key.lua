b6488d44c168d0235cc16bb18c76ed250d54026d
jdhao <jdhao@hotmail.com>
2021-10-17 07:50:38 +0800

    which-key conf

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -40,10 +40,10 @@ require("which-key").setup({
     padding = { 1, 0, 1, 0 }, -- extra window padding [top, right, bottom, left]
   },
   layout = {
-    height = { min = 4, max = 25 }, -- min and max height of the columns
+    height = { min = 1, max = 25 }, -- min and max height of the columns
     width = { min = 20, max = 50 }, -- min and max width of the columns
-    spacing = 2, -- spacing between columns
-    align = "left", -- align columns left, center or right
+    spacing = 1, -- spacing between columns
+    align = "center", -- align columns left, center or right
   },
   ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
   hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ "}, -- hide mapping boilerplate
