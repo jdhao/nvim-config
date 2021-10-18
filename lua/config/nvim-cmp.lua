7750a43e55527461822b1538a9342c49d8270e03
jdhao <jdhao@hotmail.com>
2021-10-19 01:14:09 +0800

    update cmp settings

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -24,6 +24,9 @@ cmp.setup({
       end
     end,
     ['<Esc>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
   },
   sources = {
     { name = 'nvim_lsp' }, -- For nvim-lsp
@@ -33,7 +36,7 @@ cmp.setup({
   },
   completion = {
     keyword_length = 1,
-    completeopt = "menu,menuone,noinsert"
+    completeopt = "menu,noselect"
   },
   experimental = {
     ghost_text = false
