66be7b937999ea462207e5f48a9a27caaf0c740a
jdhao <jdhao@hotmail.com>
2021-10-18 22:32:58 +0800

    use local variable

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -65,7 +65,7 @@ local custom_attach = function(client, bufnr)
   vim.notify(msg, 'info', {title = 'Nvim-config'})
 end
 
-capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local lspconfig = require("lspconfig")
