49482b8ea010329ab56eb29a5af79d161df0f892
jdhao <jdhao@hotmail.com>
2021-10-29 23:33:57 +0800

    change hop.nvim highlight

--- lua/config/nvim_hop.lua
+++ lua/config/nvim_hop.lua
@@ -1,3 +1,4 @@
+vim.cmd[[ hi HopNextKey cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff ]]
 require('hop').setup()
 
 vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_char2()<cr>", {noremap = true})
