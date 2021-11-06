538afe2fc65f4c56d62f8eb8bcb0a03e1c53dc32
jdhao <jdhao@hotmail.com>
2021-11-06 13:09:50 +0800

    update indent-blankline conf

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -1,16 +1,17 @@
-require("indent_blankline").setup {
-  -- U+2502 may also be a good choice, it will be on the middle of cursor.
-  char = "▏",
-  show_end_of_line = false,
-  disable_with_nolist = true,
-  buftype_exclude = {"terminal"},
-  filetype_exclude = { "help", "git", "markdown", "snippets", "text" },
-}
+require("indent_blankline").setup({
+	-- U+2502 may also be a good choice, it will be on the middle of cursor.
+	-- U+250A is also a good choice
+	char = "▏",
+	show_end_of_line = false,
+	disable_with_nolist = true,
+	buftype_exclude = { "terminal" },
+	filetype_exclude = { "help", "git", "markdown", "snippets", "text", "gitconfig" },
+})
 
-vim.cmd[[
+vim.cmd([[
 augroup indent_blankline
   autocmd!
   autocmd InsertEnter * IndentBlanklineDisable
   autocmd InsertLeave * IndentBlanklineEnable
 augroup END
-]]
+]])
