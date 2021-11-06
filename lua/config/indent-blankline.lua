62b62265203a9c2949ec80ae205b350c37fd2fc8
jdhao <jdhao@hotmail.com>
2021-11-06 23:10:24 +0800

    fix indent-blankline activation bug
    When InsertLeave is triggered, we should check if we need to start
    indent-blankline before enabling it.

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -1,17 +1,23 @@
 require("indent_blankline").setup({
-	-- U+2502 may also be a good choice, it will be on the middle of cursor.
-	-- U+250A is also a good choice
-	char = "▏",
-	show_end_of_line = false,
-	disable_with_nolist = true,
-	buftype_exclude = { "terminal" },
-	filetype_exclude = { "help", "git", "markdown", "snippets", "text", "gitconfig" },
+  -- U+2502 may also be a good choice, it will be on the middle of cursor.
+  -- U+250A is also a good choice
+  char = "▏",
+  show_end_of_line = false,
+  disable_with_nolist = true,
+  buftype_exclude = { "terminal" },
+  filetype_exclude = { "help", "git", "markdown", "snippets","text", "gitconfig" },
 })
 
 vim.cmd([[
+function! Should_activate_indentblankline() abort
+  if index(g:indent_blankline_filetype_exclude, &filetype) == -1
+    IndentBlanklineEnable
+  endif
+endfunction
+
 augroup indent_blankline
   autocmd!
   autocmd InsertEnter * IndentBlanklineDisable
-  autocmd InsertLeave * IndentBlanklineEnable
+  autocmd InsertLeave * call Should_activate_indentblankline()
 augroup END
 ]])
