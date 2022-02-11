528aa2f19ad4aa6f6b53080d4f523ee1b3e15b86
jdhao <jdhao@hotmail.com>
2022-02-11 20:15:07 +0800

    update nvim-cmp conf

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -41,8 +41,8 @@ cmp.setup({
     keyword_length = 1,
     completeopt = "menu,noselect"
   },
-  experimental = {
-    ghost_text = false
+  view = {
+    entries = 'custom',
   },
   formatting = {
     format = lspkind.cmp_format({
@@ -59,4 +59,23 @@ cmp.setup({
   },
 })
 
-vim.cmd("hi link CmpItemMenu Comment")
+--  see https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#how-to-add-visual-studio-code-dark-theme-colors-to-the-menu
+vim.cmd[[
+  highlight! link CmpItemMenu Comment
+  " gray
+  highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
+  " blue
+  highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
+  highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
+  " light blue
+  highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
+  highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
+  highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
+  " pink
+  highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
+  highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
+  " front
+  highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
+  highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
+  highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
+]]
