38175254414b1aaade34152f5f819a7071586c87
jdhao <jdhao@hotmail.com>
2022-04-29 22:00:10 +0800

    update hlslens conf

--- lua/config/hlslens.lua
+++ lua/config/hlslens.lua
@@ -3,9 +3,11 @@ require('hlslens').setup({
     nearest_only = true,
 })
 
-local activate_hlslens = function(d)
-  local cmd =  "normal! " .. vim.v.count1 .. d .. "zzzv"
+local activate_hlslens = function(direction)
+  local cmd = string.format("normal! %s%szzzv", vim.v.count1, direction)
   local status, msg = pcall(vim.fn.execute, cmd)
+  -- 13 is the index where real error message starts
+  msg = msg:sub(13)
 
   if not status then
     vim.api.nvim_echo({{msg, "ErrorMsg"}}, false, {})
