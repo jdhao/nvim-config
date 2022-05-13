c3253a573142fa1e3e1ea10ed52c465eb3471ba0
jdhao <jdhao@hotmail.com>
2022-05-14 00:59:00 +0800

    use vim.pretty_print for checking lua objects

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,7 +1,6 @@
 -- inspect something
--- Taken from https://github.com/jamestthompson3/vimConfig/blob/eeef4a8eeb5a24938f8a0969a35f69c78643fb66/lua/tt/nvim_utils.lua#L106
 function inspect(item)
-  print(vim.inspect(item))
+  vim.pretty_print(item)
 end
 
 local M = {}
