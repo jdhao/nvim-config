ecde08941b72cc0dc12c30073e5fce690efc5005
jdhao <jdhao@hotmail.com>
2022-08-13 23:08:25 +0800

    update lua-language-server conf
    lua-language-server has changed its running method significantly, so we
    need to change it in the nvim-lspconfig.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -148,17 +148,13 @@ if utils.executable('bash-language-server') then
   })
 end
 
-local sumneko_binary_path = fn.exepath("lua-language-server")
-if vim.g.is_mac or vim.g.is_linux and sumneko_binary_path ~= "" then
-  local sumneko_root_path = fn.fnamemodify(sumneko_binary_path, ":h:h:h")
-
+if utils.executable("lua-language-server") then
   local runtime_path = vim.split(package.path, ";")
   table.insert(runtime_path, "lua/?.lua")
   table.insert(runtime_path, "lua/?/init.lua")
 
   lspconfig.sumneko_lua.setup({
     on_attach = custom_attach,
-    cmd = { sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua" },
     settings = {
       Lua = {
         runtime = {
