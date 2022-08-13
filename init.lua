4c96243a4c128fefc981282637cb81cad89cfde3
jdhao <jdhao@hotmail.com>
2022-08-13 22:45:13 +0800

    init.vim --> init.lua cleanup

--- init.lua
+++ init.lua
@@ -26,11 +26,9 @@ local core_conf_files = {
   "themes.vim",
 }
 
+-- source all the core config files
 for _, name in ipairs(core_conf_files) do
   local path = string.format("%s/core/%s", vim.fn.stdpath('config'), name)
   local source_cmd = "source " .. path
   vim.cmd(source_cmd)
 end
--- for s:fname in s:core_conf_files
---   execute printf('source %s/core/%s', stdpath('config'), s:fname)
--- endfor
