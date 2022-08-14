b3b69f21f4a8d0895d751d9472f84bf780e00a75
jdhao <jdhao@hotmail.com>
2022-08-15 00:22:24 +0800

    update mapping

--- lua/custom-map.lua
+++ lua/custom-map.lua
@@ -1,8 +1,11 @@
 local keymap = vim.keymap
 
--- Go to the begining and end of current line in insert mode quickly
+-- Go to the beginning and end of current line in insert mode quickly
 keymap.set('i', '<C-A>', '<HOME>')
 keymap.set('i', '<C-E>', '<END>')
+
+-- Go to beginning of command in command-line mode
+keymap.set('c', '<C-A>', '<HOME>')
 
 -- Delete the character to the right of the cursor
 keymap.set('i', '<C-D>', '<DEL>')
