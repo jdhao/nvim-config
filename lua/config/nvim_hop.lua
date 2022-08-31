16be5e0499a2569b920bb99911b76eab0353ac0b
jdhao <jdhao@hotmail.com>
2022-08-31 23:56:52 +0800

    update hop.nvim conf

--- lua/config/nvim_hop.lua
+++ lua/config/nvim_hop.lua
@@ -1,14 +1,19 @@
-vim.cmd[[ hi HopNextKey cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff ]]
-vim.cmd[[ hi HopNextKey1 cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff ]]
-vim.cmd[[ hi HopNextKey2 cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff ]]
+vim.cmd [[hi HopNextKey cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff]]
+vim.cmd [[hi HopNextKey1 cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff]]
+vim.cmd [[hi HopNextKey2 cterm=bold ctermfg=176 gui=bold guibg=#ff00ff guifg=#ffffff]]
 
-require('hop').setup({
+local hop = require "hop"
+hop.setup({
   case_insensitive = true,
-  char2_fallback_key = '<CR>',
-  quit_key='<Esc>',
+  char2_fallback_key = "<CR>",
+  quit_key = "<Esc>",
 })
 
-vim.keymap.set('n', 'f', function()
-  return require('hop').hint_char2()
-end,
-{ silent = true, noremap = true, desc = "nvim-hop char2" })
+vim.keymap.set({ "n", "v" }, "f", "", {
+  silent = true,
+  noremap = true,
+  callback = function()
+    hop.hint_char2()
+  end,
+  desc = "nvim-hop char2",
+})
