1235430439ff1c5c5b46a64a5a20bceab13dd6ce
jdhao <jdhao@hotmail.com>
2022-10-15 18:29:28 +0800

    update nvim-cmp lsp conf

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -83,9 +83,7 @@ local custom_attach = function(client, bufnr)
   end
 end
 
-local capabilities = lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local lspconfig = require("lspconfig")
 
