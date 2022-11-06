075326ee2e457b70aa9e2b50da7a87767d340e41
jdhao <jdhao@hotmail.com>
2022-11-07 00:41:59 +0800

    fix: make xkbswitch work on mac M1 CPU
    For the details, see https://jdhao.github.io/2021/02/25/nvim_ime_mode_auto_switch/.

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -8,11 +8,17 @@ local function spell()
   return ""
 end
 
+--- show indicator for Chinese IME
 local function ime_state()
   if vim.g.is_mac then
     -- ref: https://github.com/vim-airline/vim-airline/blob/master/autoload/airline/extensions/xkblayout.vim#L11
     local layout = fn.libcall(vim.g.XkbSwitchLib, "Xkb_Switch_getXkbLayout", "")
-    if layout == "0" then
+
+    -- We can use `xkbswitch -g` on the command line to get current mode.
+    -- mode for macOS builtin pinyin IME: com.apple.inputmethod.SCIM.ITABC
+    -- mode for Rime: im.rime.inputmethod.Squirrel.Rime
+    local res = fn.match(layout, [[\v(Squirrel\.Rime|SCIM.ITABC)]])
+    if res ~= -1 then
       return "[CN]"
     end
   end
