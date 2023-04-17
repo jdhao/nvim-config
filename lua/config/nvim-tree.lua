c27c61cdea97a555681c615b3afc50b2646dd09f
jdhao <jdhao@hotmail.com>
2023-04-17 22:04:33 +0200

    update nvim-tree conf

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -7,9 +7,6 @@ nvim_tree.setup {
   hijack_cursor = false,
   hijack_netrw = true,
   hijack_unnamed_buffer_when_opening = false,
-  ignore_buffer_on_setup = false,
-  open_on_setup = false,
-  open_on_setup_file = false,
   open_on_tab = false,
   sort_by = "name",
   update_cwd = false,
@@ -50,7 +47,6 @@ nvim_tree.setup {
     update_cwd = false,
     ignore_list = {},
   },
-  ignore_ft_on_setup = {},
   system_open = {
     cmd = "",
     args = {},
