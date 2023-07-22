41175cacb12fe3acc4dccbee2fa850a580ef520b
jdhao <jdhao@hotmail.com>
2023-07-22 14:44:56 +0200

    feat: add treesitter toml

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { "python", "cpp", "lua", "vim", "json" },
+  ensure_installed = { "python", "cpp", "lua", "vim", "json", "toml" },
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
