223823a284507e85879fac2213384068044bff80
jdhao <jdhao@hotmail.com>
2023-10-11 20:42:32 +0200

    Remove obsolete options for nvim-tree

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -12,7 +12,6 @@ nvim_tree.setup {
   update_cwd = false,
   view = {
     width = 30,
-    hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
     number = false,
