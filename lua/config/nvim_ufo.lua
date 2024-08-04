933b887281eaefec6f8c64c7ea627de8308caddf
jdhao <jdhao@hotmail.com>
2024-08-04 23:47:08 +0200

    update config for nvim-ufo

--- lua/config/nvim_ufo.lua
+++ lua/config/nvim_ufo.lua
@@ -40,9 +40,7 @@ vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
 vim.keymap.set("n", "<leader>K", function()
-  local winid = require("ufo").peekFoldedLinesUnderCursor()
-  if not winid then
-    -- vim.lsp.buf.hover()
-    vim.cmd([[ Lspsaga hover_doc ]])
-  end
-end)
+  local _ = require("ufo").peekFoldedLinesUnderCursor()
+end, {
+  desc = "Preview folded maps",
+})
