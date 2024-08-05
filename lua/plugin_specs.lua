ca89b2b9548c332e20b93da204d6b1e0fa67ed73
jdhao <jdhao@hotmail.com>
2024-08-06 00:01:17 +0200

    update load condtion for nvim-tree

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -527,7 +527,7 @@ local plugin_specs = {
   -- file explorer
   {
     "nvim-tree/nvim-tree.lua",
-    keys = { "<space>s" },
+    event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("config.nvim-tree")
