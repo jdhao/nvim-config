f1062a3d8f0002e8fd3ddf45580397e013c0aa8b
jdhao <jdhao@hotmail.com>
2024-08-06 18:00:41 +0200

    filter fold level sign for statuscol.nvim
    Only show fold signs for fold level below a certain level to avoid
    clutter the left side columns with a lot of folding signs

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -190,15 +190,7 @@ local plugin_specs = {
     "luukvbaal/statuscol.nvim",
     opts = {},
     config = function()
-      local builtin = require("statuscol.builtin")
-      require("statuscol").setup {
-        relculright = true,
-        segments = {
-          { text = { "%s" }, click = "v:lua.ScSa" },
-          { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-          { text = { builtin.foldfunc, " " }, condition = {true, builtin.not_empty}, click = "v:lua.ScFa" },
-        },
-      }
+      require("config.nvim-statuscol")
     end,
   },
   {
