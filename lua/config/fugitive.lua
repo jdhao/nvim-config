9ff76931e366634f0e73d0ef3ca600bb79c5272a
jdhao <jdhao@hotmail.com>
2024-08-09 22:18:39 +0200

    update git related mappings

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -1,12 +1,27 @@
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Git status" })
-keymap.set("n", "<leader>gw", "<cmd>Gwrite<cr>", { desc = "Git add" })
-keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>", { desc = "Git commit" })
-keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<cr>", { desc = "Git diff" })
-keymap.set("n", "<leader>gpl", "<cmd>Git pull<cr>", { desc = "Git pull" })
-keymap.set("n", "<leader>gpu", "<cmd>15 split|term git push<cr>", { desc = "Git push" })
-keymap.set("v", "<leader>gb", ":Git blame<cr>", { desc = "Git blame line" })
+keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Git: show status" })
+keymap.set("n", "<leader>gw", "<cmd>Gwrite<cr>", { desc = "Git: add file" })
+keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>", { desc = "Git: commit changes" })
+keymap.set("n", "<leader>gpl", "<cmd>Git pull<cr>", { desc = "Git: pull changes" })
+keymap.set("n", "<leader>gpu", "<cmd>15 split|term git push<cr>", { desc = "Git: push changes" })
+keymap.set("v", "<leader>gb", ":Git blame<cr>", { desc = "Git: blame selected line" })
 
 -- convert git to Git in command line mode
 vim.fn["utils#Cabbrev"]("git", "Git")
+
+keymap.set("n", "<leader>gnb", function()
+  vim.ui.input({ prompt = "Enter the new branch nmae" }, function(user_input)
+    if user_input == nil or user_input == "" then
+      return
+    end
+
+    local cmd_str = string.format("G checkout -b %s", user_input)
+    vim.cmd(cmd_str)
+  end)
+end, {
+  desc = "Git: create new branch",
+})
+
+keymap.set("n", "<leader>gf", "<cmd>Git fetch", { desc = "Git: prune branches" })
+keymap.set("n", "<leader>gbd", ":Git branch -D ", { desc = "Git: delete branch" })
