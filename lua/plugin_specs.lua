6e60475f3f956ee4b7a2a2deea47f44d9676ed9a
jdhao <jdhao@hotmail.com>
2024-08-16 00:02:24 +0200

    Use gx.nvim for browsing in Browser

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -223,7 +223,12 @@ local plugin_specs = {
   -- For Windows and Mac, we can open an URL in the browser. For Linux, it may
   -- not be possible since we maybe in a server which disables GUI.
   {
-    "tyru/open-browser.vim",
+    "chrishrb/gx.nvim",
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
+    init = function()
+      vim.g.netrw_nogx = 1 -- disable netrw gx
+    end,
     enabled = function()
       if vim.g.is_win or vim.g.is_mac then
         return true
@@ -231,7 +236,9 @@ local plugin_specs = {
         return false
       end
     end,
-    event = "VeryLazy",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true, -- default settings
+    submodules = false, -- not needed, submodules are required only for tests
   },
 
   -- Only install these plugins if ctags are installed on the system
