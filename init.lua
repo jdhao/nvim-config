f99acf894c6b3ed6b88efbb79577f95fdcdfe474
wei <qqwei1123@163.com>
2024-09-05 15:15:40 +0800

    add vscode config

--- init.lua
+++ init.lua
@@ -11,46 +11,47 @@
 -- StackOverflow: https://stackoverflow.com/users/6064933/jdhao
 if vim.g.vscode then
     -- VSCode extension
+   require('vscode')
 else
     -- ordinary Neovim
-vim.loader.enable()
+  vim.loader.enable()
 
-local version = vim.version
+  local version = vim.version
 
--- check if we have the latest stable version of nvim
-local expected_ver = "0.10.0"
-local ev = version.parse(expected_ver)
-local actual_ver = version()
+  -- check if we have the latest stable version of nvim
+  local expected_ver = "0.10.0"
+  local ev = version.parse(expected_ver)
+  local actual_ver = version()
 
-local result = version.cmp(ev, {actual_ver.major, actual_ver.minor, actual_ver.patch})
+  local result = version.cmp(ev, {actual_ver.major, actual_ver.minor, actual_ver.patch})
 
-if  result ~= 0 then
-  local _ver = string.format("%s.%s.%s", actual_ver.major, actual_ver.minor, actual_ver.patch)
-  local msg = string.format("Expect nvim %s, but got %s instead. Use at your own risk!", expected_ver, _ver)
-  vim.api.nvim_err_writeln(msg)
-end
-
-local core_conf_files = {
-  "globals.lua", -- some global settings
-  "options.vim", -- setting options in nvim
-  "autocommands.vim", -- various autocommands
-  "mappings.lua", -- all the user-defined mappings
-  "plugins.vim", -- all the plugins installed and their configurations
-  "colorschemes.lua", -- colorscheme settings
-}
-
-local viml_conf_dir = vim.fn.stdpath("config") .. "/viml_conf"
--- source all the core config files
-for _, file_name in ipairs(core_conf_files) do
-  if vim.endswith(file_name, 'vim') then
-    local path = string.format("%s/%s", viml_conf_dir, file_name)
-    local source_cmd = "source " .. path
-    vim.cmd(source_cmd)
-  else
-    local module_name, _ = string.gsub(file_name, "%.lua", "")
-    package.loaded[module_name] = nil
-    require(module_name)
+  if  result ~= 0 then
+    local _ver = string.format("%s.%s.%s", actual_ver.major, actual_ver.minor, actual_ver.patch)
+    local msg = string.format("Expect nvim %s, but got %s instead. Use at your own risk!", expected_ver, _ver)
+    vim.api.nvim_err_writeln(msg)
   end
-end
+
+  local core_conf_files = {
+    "globals.lua", -- some global settings
+    "options.vim", -- setting options in nvim
+    "autocommands.vim", -- various autocommands
+    "mappings.lua", -- all the user-defined mappings
+    "plugins.vim", -- all the plugins installed and their configurations
+    "colorschemes.lua", -- colorscheme settings
+  }
+
+  local viml_conf_dir = vim.fn.stdpath("config") .. "/viml_conf"
+  -- source all the core config files
+  for _, file_name in ipairs(core_conf_files) do
+    if vim.endswith(file_name, 'vim') then
+      local path = string.format("%s/%s", viml_conf_dir, file_name)
+      local source_cmd = "source " .. path
+      vim.cmd(source_cmd)
+    else
+      local module_name, _ = string.gsub(file_name, "%.lua", "")
+      package.loaded[module_name] = nil
+      require(module_name)
+    end
+  end
 
 end
