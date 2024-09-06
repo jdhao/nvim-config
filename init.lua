796c490c9129b0798c8f13c3cbf2e2e9fed50e7a
wei <qqwei1123@163.com>
2024-09-06 14:43:14 +0800

    update version

--- init.lua
+++ init.lua
@@ -15,22 +15,6 @@ if vim.g.vscode then
 else
     -- ordinary Neovim
   vim.loader.enable()
-
-  local version = vim.version
-
-  -- check if we have the latest stable version of nvim
-  local expected_ver = "0.10.0"
-  local ev = version.parse(expected_ver)
-  local actual_ver = version()
-
-  local result = version.cmp(ev, {actual_ver.major, actual_ver.minor, actual_ver.patch})
-
-  if  result ~= 0 then
-    local _ver = string.format("%s.%s.%s", actual_ver.major, actual_ver.minor, actual_ver.patch)
-    local msg = string.format("Expect nvim %s, but got %s instead. Use at your own risk!", expected_ver, _ver)
-    vim.api.nvim_err_writeln(msg)
-  end
-
   local core_conf_files = {
     "globals.lua", -- some global settings
     "options.vim", -- setting options in nvim
