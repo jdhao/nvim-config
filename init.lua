cc03a711c93e01bb75d3cf59f3db8b76029b37ef
jdhao <jdhao@hotmail.com>
2024-10-09 17:58:18 +0200

    update supported nvim version to 0.10.2

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ vim.loader.enable()
 
 local utils = require("utils")
 
-local expected_version = "0.10.1"
+local expected_version = "0.10.2"
 utils.is_compatible_version(expected_version)
 
 local config_dir = vim.fn.stdpath("config")
