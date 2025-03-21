8cba1ba234604069cee272eb55649203c21d9dde
Okarin <kleinmond@hotmail.de>
2025-03-21 14:42:33 +0100

    Install treesitter on all systems (#384)
    For some reason treesitter gets only installed on mac os. This conflicts with treesitter-textobjects which gets installed for every system.

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -50,12 +50,6 @@ local plugin_specs = {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    enabled = function()
-      if vim.g.is_mac then
-        return true
-      end
-      return false
-    end,
     event = "VeryLazy",
     build = ":TSUpdate",
     config = function()
