be4bcc30f6277d78cc4232b5a6cd55eac7ec4896
jdhao <jdhao@hotmail.com>
2025-03-30 20:57:45 +0200

    use builtin log level enum to specify log level

--- lua/globals.lua
+++ lua/globals.lua
@@ -10,7 +10,7 @@ vim.g.is_win = (utils.has("win32") or utils.has("win64")) and true or false
 vim.g.is_linux = (utils.has("unix") and (not utils.has("macunix"))) and true or false
 vim.g.is_mac = utils.has("macunix") and true or false
 
-vim.g.logging_level = "info"
+vim.g.logging_level = vim.log.levels.INFO
 
 ------------------------------------------------------------------------
 --                         builtin variables                          --
