ae35a205f38ff0954a8ac8e97801fbc82667bc36
jdhao <jdhao@hotmail.com>
2025-04-04 18:15:21 +0200

    remove plugin vim-markdown

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -115,18 +115,7 @@ local plugin_specs = {
     -- optional for icon support
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("fzf-lua").setup {
-        winopts = {
-          row = 0.5,
-          height = 0.7,
-        },
-      }
-
-      vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fuzzy find files" })
-      vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "Fuzzy grep files" })
-      vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua helptags<cr>", { desc = "Fuzzy grep tags in help files" })
-      vim.keymap.set("n", "<leader>ft", "<cmd>FzfLua btags<cr>", { desc = "Fuzzy search buffer tags" })
-      vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Fuzzy search opened buffers" })
+      require("config.fzf-lua")
     end,
   },
   {
@@ -346,9 +335,6 @@ local plugin_specs = {
       require("config.bqf")
     end,
   },
-
-  -- Another markdown plugin
-  -- { "preservim/vim-markdown", ft = { "markdown" } },
 
   -- Faster footnote generation
   { "vim-pandoc/vim-markdownfootnotes", ft = { "markdown" } },
