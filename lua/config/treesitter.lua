0d9080cf74f3fa28896adfda7e7e20064930676d
Ricardo Antolin <ricardo.antolin@cabify.com>
2025-04-12 19:10:52 +0200

    deprecation fixes, adds ruby-lsp, treesitter and codeium, deletes copilot

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,6 +1,7 @@
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { "python", "cpp", "lua", "vim", "json", "toml" },
+  ensure_installed = { "python", "cpp", "lua", "vim", "json", "toml", "go", "dart", "ruby" },
   ignore_install = {}, -- List of parsers to ignore installing
+  auto_install = true,
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "help" }, -- list of language that will be disabled
