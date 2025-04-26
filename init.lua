88392014500313aa1f16c49e2e34c54217c41ce6
jdhao <jdhao@hotmail.com>
2025-04-26 21:48:18 +0200

    upgrade nvim to 0.11.1 (#404)

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ vim.loader.enable()
 
 local utils = require("utils")
 
-local expected_version = "0.11.0"
+local expected_version = "0.11.1"
 utils.is_compatible_version(expected_version)
 
 local config_dir = vim.fn.stdpath("config")
