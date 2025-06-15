2293a0d45683f0989d3b966a39dcfbb47a487294
jdhao <jdhao@hotmail.com>
2025-06-16 01:04:56 +0200

    Move blink-cmp config to dedicated file (#418)

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -36,69 +36,17 @@ local plugin_specs = {
       require("config.nvim-cmp")
     end,
   },
-  --{
-  --  "saghen/blink.cmp",
-  --  -- optional: provides snippets for the snippet source
-  --  dependencies = { "rafamadriz/friendly-snippets" },
-
-  --  -- use a release tag to download pre-built binaries
-  --  version = "1.*",
-
-  --  ---@module 'blink.cmp'
-  --  ---@type blink.cmp.Config
-  --  opts = {
-  --    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-  --    -- 'super-tab' for mappings similar to vscode (tab to accept)
-  --    -- 'enter' for enter to accept
-  --    -- 'none' for no mappings
-  --    --
-  --    keymap = {
-  --      preset = "default",
-  --      ["<Tab>"] = { "select_next", "fallback" },
-  --      ["<S-Tab>"] = { "select_prev", "fallback" },
-  --      ["<Enter>"] = { "select_and_accept", "fallback" },
-  --      ["<C-U>"] = { "scroll_documentation_up", "fallback" },
-  --      ["<C-D>"] = { "scroll_documentation_down", "fallback" },
-  --    },
-
-  --    appearance = {
-  --      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-  --      -- Adjusts spacing to ensure icons are aligned
-  --      nerd_font_variant = "mono",
-  --    },
-
-  --    -- (Default) Only show the documentation popup when manually triggered
-  --    completion = {
-  --      documentation = {
-  --        auto_show = true,
-  --      },
-  --    },
-
-  --    -- Default list of enabled providers defined so that you can extend it
-  --    -- elsewhere in your config, without redefining it, due to `opts_extend`
-  --    sources = {
-  --      default = { "lsp", "path", "buffer" },
-  --    },
-
-  --    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-  --    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-  --    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-  --    --
-  --    -- See the fuzzy documentation for more information
-  --    fuzzy = { implementation = "prefer_rust_with_warning" },
-  --    cmdline = {
-  --      completion = {
-  --        menu = {
-  --          auto_show = true,
-  --        },
-  --      },
-  --      keymap = {
-  --        ["<Enter>"] = { "select_and_accept", "fallback" },
-  --      },
-  --    },
-  --  },
-  --  opts_extend = { "sources.default" },
-  --},
+  -- {
+  --   "saghen/blink.cmp",
+  --   -- optional: provides snippets for the snippet source
+  --   dependencies = { "rafamadriz/friendly-snippets" },
+  --   -- use a release tag to download pre-built binaries
+  --   version = "1.*",
+  --   config = function()
+  --     require("config.blink-cmp")
+  --   end,
+  --   opts_extend = { "sources.default" },
+  -- },
   {
     "neovim/nvim-lspconfig",
     config = function()
