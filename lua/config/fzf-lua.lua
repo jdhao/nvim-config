af84b8d51123852c020153579338d7db36f2efd5
Ruben De Raeve <rderaeve@gmail.com>
2025-08-18 09:04:14 +0200

    [fzf-lua] fix recently opened files
    refs [issue 426](https://github.com/jdhao/nvim-config/issues/426)

--- lua/config/fzf-lua.lua
+++ lua/config/fzf-lua.lua
@@ -16,3 +16,4 @@ vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "Fuzzy g
 vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua helptags<cr>", { desc = "Fuzzy grep tags in help files" })
 vim.keymap.set("n", "<leader>ft", "<cmd>FzfLua btags<cr>", { desc = "Fuzzy search buffer tags" })
 vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Fuzzy search opened buffers" })
+vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua oldfiles<cr>", { desc = "Fuzzy search opened files history" })
