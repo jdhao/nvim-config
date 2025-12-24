d6f7dde87591d43a3902d92b4f6f13bafd047877
jdhao <jdhao@hotmail.com>
2025-12-24 22:27:36 +0100

    update stylua config

--- lua/config/fzf-lua.lua
+++ lua/config/fzf-lua.lua
@@ -19,7 +19,22 @@ require("fzf-lua").setup {
 
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fuzzy find files" })
 vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "Fuzzy grep files" })
-vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua helptags<cr>", { desc = "Fuzzy grep tags in help files" })
+vim.keymap.set(
+  "n",
+  "<leader>fh",
+  "<cmd>FzfLua helptags<cr>",
+  { desc = "Fuzzy grep tags in help files" }
+)
 vim.keymap.set("n", "<leader>ft", "<cmd>FzfLua btags<cr>", { desc = "Fuzzy search buffer tags" })
-vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Fuzzy search opened buffers" })
-vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua oldfiles<cr>", { desc = "Fuzzy search opened files history" })
+vim.keymap.set(
+  "n",
+  "<leader>fb",
+  "<cmd>FzfLua buffers<cr>",
+  { desc = "Fuzzy search opened buffers" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>fr",
+  "<cmd>FzfLua oldfiles<cr>",
+  { desc = "Fuzzy search opened files history" }
+)
