bca43f3bacdc30ee15523d539ce858136e0a43c3
jdhao <jdhao@hotmail.com>
2026-02-01 22:48:15 +0100

    feat: switch to main branch of nvim-treesitter
    Note:
    
    - tree-sitter-cli needs to be installed (in macOS, run `brew install tree-sitter-cli`)
    - if nvim-treesitter and nvim-treesitter-textobjects are using the old
      master branch, we need to delete the old plugin and then re-install
      them to have the main branch

--- lua/config/treesitter-textobjects.lua
+++ lua/config/treesitter-textobjects.lua
@@ -1,44 +1,50 @@
-require("nvim-treesitter.configs").setup {
-  textobjects = {
-    select = {
-      enable = true,
-
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
-
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        -- You can optionally set descriptions to the mappings (used in the desc parameter of
-        -- nvim_buf_set_keymap) which plugins like which-key display
-        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-      },
-      -- You can choose the select mode (default is charwise 'v')
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * method: eg 'v' or 'o'
-      -- and should return the mode ('v', 'V', or '<c-v>') or a table
-      -- mapping query_strings to modes.
-      selection_modes = {
-        ["@function.inner"] = "V", -- linewise
-        ["@function.outer"] = "V", -- linewise
-        ["@class.outer"] = "V", -- linewise
-        ["@class.inner"] = "V", -- linewise
-        ["@parameter.outer"] = "v", -- charwise
-      },
-      -- If you set this to `true` (default is `false`) then any textobject is
-      -- extended to include preceding or succeeding whitespace. Succeeding
-      -- whitespace has priority in order to act similarly to eg the built-in
-      -- `ap`.
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * selection_mode: eg 'v'
-      -- and should return true or false
-      include_surrounding_whitespace = false,
+-- configuration
+require("nvim-treesitter-textobjects").setup {
+  select = {
+    -- Automatically jump forward to textobj, similar to targets.vim
+    lookahead = true,
+    -- You can choose the select mode (default is charwise 'v')
+    --
+    -- Can also be a function which gets passed a table with the keys
+    -- * query_string: eg '@function.inner'
+    -- * method: eg 'v' or 'o'
+    -- and should return the mode ('v', 'V', or '<c-v>') or a table
+    -- mapping query_strings to modes.
+    selection_modes = {
+      ["@function.inner"] = "V", -- linewise
+      ["@function.outer"] = "V", -- linewise
+      ["@class.outer"] = "V", -- linewise
+      ["@class.inner"] = "V", -- linewise
+      ["@parameter.outer"] = "v", -- charwise
     },
+    -- If you set this to `true` (default is `false`) then any textobject is
+    -- extended to include preceding or succeeding whitespace. Succeeding
+    -- whitespace has priority in order to act similarly to eg the built-in
+    -- `ap`.
+    --
+    -- Can also be a function which gets passed a table with the keys
+    -- * query_string: eg '@function.inner'
+    -- * selection_mode: eg 'v'
+    -- and should return true of false
+    include_surrounding_whitespace = false,
   },
 }
+
+-- keymaps
+-- You can use the capture groups defined in `textobjects.scm`
+vim.keymap.set({ "x", "o" }, "af", function()
+  require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "if", function()
+  require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "ac", function()
+  require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "ic", function()
+  require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+end)
+-- You can also use captures from other query groups like `locals.scm`
+vim.keymap.set({ "x", "o" }, "as", function()
+  require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
+end)
--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,8 +1,52 @@
-require("nvim-treesitter.configs").setup {
-  ensure_installed = { "python", "cpp", "lua", "vim", "json", "toml" },
-  ignore_install = {}, -- List of parsers to ignore installing
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "help" }, -- list of language that will be disabled
-  },
+-- a list of filetypes to install treesitter parsers and queries
+local ensure_installed = {
+  "python",
+  "cpp",
+  "lua",
+  "vim",
+  "json",
+  "toml",
+  "yaml",
+  "javascript",
+  "go",
+  "typescript",
+  "markdown",
+  "sh",
+  "zsh",
 }
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = ensure_installed,
+
+  callback = function(args)
+    local ft = vim.bo[args.buf].filetype
+    local lang = vim.treesitter.language.get_lang(ft)
+
+    -- check if parser is available
+    if not vim.treesitter.language.add(lang) then
+      local available = vim.g.ts_available or require("nvim-treesitter").get_available()
+      if not vim.g.ts_available then
+        vim.g.ts_available = available
+      end
+
+      if vim.tbl_contains(available, lang) then
+        -- install treesitter parsers and queries
+        local install_msg = string.format("Installing parsers and queries for %s", lang)
+        vim.print(install_msg)
+        require("nvim-treesitter").install(lang)
+      end
+    end
+
+    if vim.treesitter.language.add(lang) then
+      -- start treesitter highlighting
+      vim.treesitter.start(args.buf, lang)
+
+      -- the following two statements will enable treesitter folding
+      -- vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      -- vim.wo[0][0].foldmethod = "expr"
+
+      -- enable treesitter-based indentation
+      -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end
+  end,
+})
