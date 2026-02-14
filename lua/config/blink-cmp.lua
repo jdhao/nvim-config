63799b5e066d8519bfb416dc1fab76483ff9269f
jdhao <jdhao@hotmail.com>
2026-02-14 12:02:26 +0100

    update conf for blink-cmp

--- lua/config/blink-cmp.lua
+++ lua/config/blink-cmp.lua
@@ -29,7 +29,56 @@ require("blink.cmp").setup {
   -- Default list of enabled providers defined so that you can extend it
   -- elsewhere in your config, without redefining it, due to `opts_extend`
   sources = {
-    default = { "lsp", "path", "buffer" },
+    default = { "lsp", "path", "buffer", "omni" },
+    providers = {
+      -- Use the thesaurus source
+      thesaurus = {
+        name = "blink-cmp-words",
+        module = "blink-cmp-words.thesaurus",
+        -- All available options
+        opts = {
+          -- A score offset applied to returned items.
+          -- By default the highest score is 0 (item 1 has a score of -1, item 2 of -2 etc..).
+          score_offset = 0,
+
+          -- Default pointers define the lexical relations listed under each definition,
+          -- see Pointer Symbols below.
+          -- Default is as below ("antonyms", "similar to" and "also see").
+          definition_pointers = { "!", "&", "^" },
+
+          -- The pointers that are considered similar words when using the thesaurus,
+          -- see Pointer Symbols below.
+          -- Default is as below ("similar to", "also see" }
+          similarity_pointers = { "&", "^" },
+
+          -- The depth of similar words to recurse when collecting synonyms. 1 is similar words,
+          -- 2 is similar words of similar words, etc. Increasing this may slow results.
+          similarity_depth = 2,
+        },
+      },
+
+      -- Use the dictionary source
+      dictionary = {
+        name = "blink-cmp-words",
+        module = "blink-cmp-words.dictionary",
+        -- All available options
+        opts = {
+          -- The number of characters required to trigger completion.
+          -- Set this higher if completion is slow, 3 is default.
+          dictionary_search_threshold = 3,
+
+          -- See above
+          score_offset = 0,
+
+          -- See above
+          definition_pointers = { "!", "&", "^" },
+        },
+      },
+    },
+    per_filetype = {
+      text = { "dictionary" },
+      markdown = { "dictionary" },
+    },
   },
 
   -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
