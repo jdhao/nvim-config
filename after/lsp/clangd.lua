2c2354d9a27773f9991791618f5fcd89b5bc11a3
jdhao <jdhao@hotmail.com>
2026-03-15 19:46:23 +0100

    Add completion support for lsp settings (#473)
    For more details, see https://www.reddit.com/r/neovim/comments/1rssfs9/a_new_use_for_lspconfig_providing_types_for_your/

--- after/lsp/clangd.lua
+++ after/lsp/clangd.lua
@@ -1,3 +1,4 @@
+---@type vim.lsp.Config
 return {
   filetypes = { "c", "cpp", "cc" },
 }
--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -698,6 +698,7 @@ local plugin_specs = {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "nvim-lspconfig", words = { "lspconfig" } },
       },
     },
   },
