93a6779b520a1439d7168f4c9b30964929850029
jdhao <jdhao@hotmail.com>
2026-03-21 00:19:14 +0100

    Add mapping for conflict resolution

--- lua/config/diffview.lua
+++ lua/config/diffview.lua
@@ -3,6 +3,9 @@ if not ok then
   return
 end
 
+local actions = require("diffview.actions")
+local prefix_conflicts = "<leader>gC"
+
 diffview.setup {
   enhanced_diff_hl = true,
   view = {
@@ -18,6 +21,31 @@ diffview.setup {
       type = "split",
       position = "bottom",
       height = 10,
+    },
+  },
+  keymaps = {
+    view = {
+      {
+        "n",
+        prefix_conflicts .. "t",
+        actions.conflict_choose("theirs"),
+        { desc = "Conflict choose theirs" },
+      },
+      {
+        "n",
+        prefix_conflicts .. "o",
+        actions.conflict_choose("ours"),
+        { desc = "Conflict choose ours" },
+      },
+      {
+        "n",
+        prefix_conflicts .. "a",
+        actions.conflict_choose("all"),
+        { desc = "Conflict choose both" },
+      },
+
+      { "n", "]C", actions.next_conflict, { desc = "Next conflict" } },
+      { "n", "[C", actions.prev_conflict, { desc = "Previous conflict" } },
     },
   },
 }
