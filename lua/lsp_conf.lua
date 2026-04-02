7851419d2a0df4a5ca92f72da5ae844ab017a39d
jdhao <jdhao@hotmail.com>
2026-04-02 19:22:09 +0200

    show LSP progress in supported terminal

--- lua/lsp_conf.lua
+++ lua/lsp_conf.lua
@@ -146,3 +146,18 @@ end, {
 vim.api.nvim_create_user_command("LspRestart", "lsp restart", {
   desc = "Restart LSP",
 })
+
+--- show LSP progress (works on Ghostty)
+vim.api.nvim_create_autocmd("LspProgress", {
+  callback = function(ev)
+    local value = ev.data.params.value
+    vim.api.nvim_echo({ { value.message or "done" } }, false, {
+      id = "lsp." .. ev.data.client_id,
+      kind = "progress",
+      source = "vim.lsp",
+      title = value.title,
+      status = value.kind ~= "end" and "running" or "success",
+      percent = value.percentage,
+    })
+  end,
+})
