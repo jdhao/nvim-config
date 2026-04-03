0ce16c60964321b9f8ff0c1b5997c2ecc14fdf0e
jdhao <jdhao@hotmail.com>
2026-04-03 15:07:48 +0200

    use native lsp method to get lsp log path

--- lua/lsp_conf.lua
+++ lua/lsp_conf.lua
@@ -135,8 +135,7 @@ vim.api.nvim_create_user_command("LspInfo", "checkhealth vim.lsp", {
 })
 
 vim.api.nvim_create_user_command("LspLog", function(_)
-  local state_path = vim.fn.stdpath("state")
-  local log_path = vim.fs.joinpath(state_path, "lsp.log")
+  local log_path = vim.lsp.log.get_filename()
 
   vim.cmd(string.format("edit %s", log_path))
 end, {
