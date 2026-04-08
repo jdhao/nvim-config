84a991c754e9e56d86bffe0282df2d5dfdccb367
jdhao <jdhao@hotmail.com>
2026-04-09 01:01:23 +0200

    update nvim-tree conf

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -40,10 +40,6 @@ nvim_tree.setup {
     update_cwd = false,
     ignore_list = {},
   },
-  system_open = {
-    cmd = "",
-    args = {},
-  },
   diagnostics = {
     enable = false,
     show_on_dirs = false,
