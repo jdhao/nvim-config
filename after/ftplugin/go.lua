2797c1ef6ab2bc57e6bb2df71040817574117c18
jdhao <jdhao@hotmail.com>
2026-04-10 00:01:41 +0200

    update tab settings for golang

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,3 +1,11 @@
+local opt = vim.opt
+
+-- General tab settings
+opt.tabstop = 4 -- Number of visual spaces per TAB
+opt.softtabstop = 4 -- Number of spaces in tab when editing
+opt.shiftwidth = 4 -- Number of spaces to use for autoindent
+opt.expandtab = false -- Expand tab to spaces so that tabs are spaces
+
 -- gofumpt adds more rule to gofmt, and is compatible with gofmt,
 -- so also https://github.com/mvdan/gofumpt
 vim.keymap.set("n", "<Space>f", function()
