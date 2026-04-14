53e2428ead723672a7858eefbcdfc43c7b9578ff
jdhao <jdhao@hotmail.com>
2026-04-14 23:10:32 +0200

    update ui2 config

--- lua/ui.lua
+++ lua/ui.lua
@@ -129,23 +129,22 @@ M.rand_colorscheme()
 require("vim._core.ui2").enable {
   enable = true,
   msg = { -- Options related to the message module.
-    ---@type 'cmd'|'msg' Default message target, either in the
-    ---cmdline or in a separate ephemeral message window.
-    ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
-    ---or table mapping |ui-messages| kinds and triggers to a target.
-    targets = "cmd",
+    targets = {
+      [""] = "cmd",
+      empty = "msg",
+    },
     cmd = { -- Options related to messages in the cmdline window.
-      height = 0.5, -- Maximum height while expanded for messages beyond 'cmdheight'.
+      height = 0.2, -- Maximum height while expanded for messages beyond 'cmdheight'.
     },
     dialog = { -- Options related to dialog window.
-      height = 0.5, -- Maximum height.
+      height = 0.2, -- Maximum height.
     },
     msg = { -- Options related to msg window.
-      height = 0.5, -- Maximum height.
-      timeout = 4000, -- Time a message is visible in the message window.
+      height = 0.2, -- Maximum height.
+      timeout = 1000, -- Time a message is visible in the message window.
     },
     pager = { -- Options related to message window.
-      height = 0.5, -- Maximum height.
+      height = 0.3, -- Maximum height.
     },
   },
 }
