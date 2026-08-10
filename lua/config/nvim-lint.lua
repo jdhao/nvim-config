98355598fc36829c200523d6e2bf960c5fd8f05d
jdhao <jdhao@hotmail.com>
2026-08-11 00:57:01 +0200

    update nvim-lint config

--- lua/config/nvim-lint.lua
+++ lua/config/nvim-lint.lua
@@ -4,10 +4,11 @@ nvim_lint.linters_by_ft = {
   -- to use vale, install it: `brew install vale`
   -- configure it via: https://vale.sh/generator, put config in `.vale.ini` in project root.
   -- to install cspell and typos: `brew install cspell typos-cli`
-  -- markdown = { "vale", "cspell", "typos" },
+  markdown = { "typos" },
+  lua = { "typos" },
 }
 
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+vim.api.nvim_create_autocmd({ "BufReadPost", "InsertLeave" }, {
   callback = function()
     -- try_lint without arguments runs the linters defined in `linters_by_ft`
     -- for the current filetype
