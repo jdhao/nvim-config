9aadce93c4c22df672116c868e77c60490e4f483
jdhao <jdhao@hotmail.com>
2026-09-09 00:33:49 +0200

    update gopls conf

--- after/lsp/gopls.lua
+++ after/lsp/gopls.lua
@@ -9,6 +9,7 @@ return {
       },
       staticcheck = true,
       gofumpt = true,
+      semanticTokens = true,
       -- inlayHints settings, see https://go.dev/gopls/inlayHints
       hints = {
         compositeLiteralFields = true,
